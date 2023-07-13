3f84e3ab99c979ad5553815375a4fc0967bf0ef8
Mr.Bread <salekbenelhabchi@gmail.com>
2023-07-13 15:00:46 +0100

    with proper grammar, but the outcome is a piece of art,\n

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - prints the sentence
+  *
+  * Return: 0 Always == Success
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
