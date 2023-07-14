7cf97fe4a14646665fb7dcc30227d957fce74eb9
Mr.Bread <salekbenelhabchi@gmail.com>
2023-07-14 17:54:21 +0100

    a wise man once said when you're confused use two while statements

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+  * main - prints all digits from 0 - 9
+  *
+  * Return: 0 Always -> Success
+  */
+int main(void)
+{
+	int n;
+
+	n = 48;
+	while (n <= 57 && n >= 48)
+	{
+		putchar(n);
+		n++;
+	}
+	n = 97;
+	while (n <= 102 && n >= 97)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
