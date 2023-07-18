aa06c828738166332b4ca2d14a0c6c2cc7aea992
Mr. Bread <salekbenelhabchi@gmail.com>
2023-07-18 19:17:46 +0100

    Handle negative numbers

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * print_last_digit - gets the last digit of a number.
+  * @n: a given number.
+  *
+  * Return: the last digit of n.
+  */
+int print_last_digit(int n)
+{
+	int lDIGIT;
+
+	if (n < 0)
+		n = n * (-1);
+	lDIGIT = n % 10;
+	_putchar(lDIGIT + '0');
+	return (lDIGIT);
+}
