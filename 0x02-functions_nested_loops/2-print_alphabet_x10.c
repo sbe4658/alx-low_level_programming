3fce7e2a368f73eb4f94e06aef3c89e9f49319ea
Mr. Bread <salekbenelhabchi@gmail.com>
2023-07-18 16:17:31 +0100

    Printing the alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * print_alphabet - prints the alphabet in lowercase followed by \n x10.
+  *
+  * Return: Nothing
+  */
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int n = 0;
+
+	while (n <= 9)
+	{
+		while (c >= 'a' && c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		c = 97;
+		_putchar('\n');
+		n++;
+	}
+}
