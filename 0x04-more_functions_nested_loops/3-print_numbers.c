564c83816032d87dcacaaa13694222966aca3ea5
Mr. Bread <salekbenelhabchi@gmail.com>
2023-07-20 21:17:30 +0100

    This one looks really familiar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints from 0 - 9.
+ *
+ * Return: Nothing.
+ */
+void print_numbers(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
