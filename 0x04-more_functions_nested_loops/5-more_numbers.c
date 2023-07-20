8484249545f9cc510309fd967a58fcdd5aafd37b
Mr. Bread <salekbenelhabchi@gmail.com>
2023-07-20 21:53:20 +0100

    Add c program to print from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * more_numbers - prints from 0 - 14, 10 times.
+ *
+ * Return: Nothing.
+ */
+
+void more_numbers(void)
+{
+	int n, m;
+
+	n = 0;
+	while (n <= 9)
+	{
+		m = 0;
+		while (m <= 14)
+		{
+			if (m >= 10)
+			{
+				_putchar((m / 10) + 48);
+				_putchar((m % 10) + 48);
+			}
+			else
+				_putchar(m + 48);
+			m++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
