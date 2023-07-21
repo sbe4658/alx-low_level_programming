7a919f7663d0c72ce843177e4ca1d72d45caae84
Mr. Bread <salekbenelhabchi@gmail.com>
2023-07-21 12:22:14 +0100

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * main - Fizz-Buzz game
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n = 1;
+
+	while (n < 100)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz ");
+		else if (n % 3 == 0)
+			printf("Fizz ");
+		else if (n % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
