d883d5db2ba78f83505f70d61af1ed1f2852071f
Mr. Bread <salekbenelhabchi@gmail.com>
2023-07-22 23:37:26 +0100

    Fixed

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-	int n = 0 , c = 0;
+	int n = 0, c = 0;
 
 	while (n <= 1024)
 	{
-		if (n % 3 || n % 5)
+		if (n % 3 == 0 || n % 5 == 0)
 		{
-			c++;
+			c = c + n;
 		}
 		n++;
 	}
