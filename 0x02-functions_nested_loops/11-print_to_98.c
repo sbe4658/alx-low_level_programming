569bcd38b959ec22457b41a2ce39ac76e1932b4e
Mr. Bread <salekbenelhabchi@gmail.com>
2023-07-22 21:50:24 +0100

    I didn't notice the stlib part XD

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print to 98.
+ * @n: given number to count from.
+ *
+ * Return: Nothing.
+ */
+
+void print_to_98(int n)
+{
+	if (n != 98)
+	{
+		if (n < 98)
+		{
+			while (n < 98)
+			{
+				printf("%d, ", n);
+				n++;
+			}
+		}
+		else
+		{
+			while (n > 98)
+			{
+				printf("%d ", n);
+				n--;
+			}
+		}
+	}
+	printf("%d\n", 98);
+}
