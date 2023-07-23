d90b12ca69444278c552c2a5593aa0ed6cc9a133
Mr. Bread <salekbenelhabchi@gmail.com>
2023-07-23 14:04:30 +0100

    Fibonacci i might be forgetting two nums

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the first 50 fibonacci numbers.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int c = 0;
+	long unsigned int n = 0, m = 1, sum;
+
+	while (c <= 50)
+	{
+		sum = n + m;
+		n = m;
+		m = sum;
+		if (c != 50)
+			printf("%ld, ", sum);
+		else
+			printf("%ld\n", sum);
+		c++;
+	}
+	return (0);
+}
