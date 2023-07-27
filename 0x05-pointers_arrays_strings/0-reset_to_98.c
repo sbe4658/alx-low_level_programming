88451022091252502d4a40d5a021eb747dbe8823
Mr. Bread <salekbenelhabchi@gmail.com>
2023-07-27 16:48:21 +0100

    Add header file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - reset a given number to 98.
+ * @n: is pointer to an int variable.
+ *
+ * Return: Nothing.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
