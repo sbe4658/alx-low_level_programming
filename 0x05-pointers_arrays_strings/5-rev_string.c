7c991a39724040bd13f1543bca4b8ffcf0d5993f
Mr. Bread <salekbenelhabchi@gmail.com>
2023-07-27 21:31:03 +0100

    i dunno why

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - reverse a given string.
+ * @s: string.
+ *
+ * Return: Nothing.
+ */
+void rev_string(char *s)
+{
+	char *tmp = s;
+	int c = 0, i = 0;
+
+	while (tmp[c] != '\0')
+		c++;
+	while (c >= 0)
+	{
+		s[c] = tmp[i];
+		c--;
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ void swap_int(int *a, int *b);
 void _puts(char *s);
 int _strlen(char *s);
 void print_rev(char *s);
+void rev_string(char *s);
 
 #endif
