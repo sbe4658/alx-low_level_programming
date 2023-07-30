d4d040c04b7fd3301edb105562d946fd9a7a65cf
Mr. Bread <salekbenelhabchi@gmail.com>
2023-07-30 22:38:57 +0100

    This is fun when you know what you're doing

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,19 +1,26 @@
 #include "main.h"
 /**
- * _strcpy - copies n bytes from a string.
- * @src: the string to be copeid.
- * @dest: destination.
- * @n: The number of bytes to be copied.
+ * _strncpy - copies n bytes from a string.
+ * @dest: the first pointer to a string will hold them both.
+ * @src: 2nd pointer to a string.
+ * @n: the numeber of bytes from src.
  *
- * Return: a char pointer dest.
+ * Return: A pointer to a string that's holds them both 'dest'.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
+	char *tmp = dest;
 	int i = 0;
 
-	while (i < n && src[i] != '\0')
+	while (i < n)
 	{
 		dest[i] = src[i];
+		i++;
+	}
+	while (tmp[n] != '\0')
+	{
+		dest[i] = tmp[n];
+		n++;
 		i++;
 	}
 	dest[i] = '\0';
