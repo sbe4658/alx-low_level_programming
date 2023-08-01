6419f9b61ee2690c129f60bfd45ea66134fbd865
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-01 14:37:32 +0100

    Fixed

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _strspn - gets the length of prefix substring, check characters
- * 	     from s againt accept.
+ * _strspn - gets the length of prefix substring, check character
+ * from s againt accept.
  * @s: char pointer to a string.
  * @accept: a char pointer to a string contains chars to compute.
  *
@@ -9,23 +9,24 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int n = 1, i = 0, c = 0, len = 0;
+	unsigned int n = 0, i = 0;
 
-	while (s[len] != '\0')
-		len++;
-	while (accept[c] != '\0')
+	while (*accept)
 	{
-		if (i == len)
+		if (*s == '\0')
 		{
 			i = 0;
-			c++;
+			accept++;
 		}
-		else if (s[i] == accept[c])
+		else if (*accept == s[i])
 		{
+			i = 0;
 			n++;
-			c++;
+			accept++;
 		}
 		i++;
 	}
-	return (n);
+	if (n > 0)
+		return (n + 1);
+	return (0);
 }
