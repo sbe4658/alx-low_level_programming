a1fafcf9e488b4b553f0de19cff6a347435819f3
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-04 18:41:11 +0100

    prints the number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - prints the number of args passed to it.
+ * @argc: the size of argv.
+ * @argv: array of strings.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, __attribute__((unused)) char **argv)
+{
+	if (argc >= 10)
+	{
+		_putchar((argc / 10) + 48);
+		_putchar((argc % 10) + 48);
+	}
+	else
+		_putchar(argc + 48);
+	_putchar('\n');
+	return (0);
+}
