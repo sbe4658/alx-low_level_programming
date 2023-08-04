5873b68a69dcfc2af79a38a7ac99896a6ee2a270
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-04 18:59:29 +0100

    the standard lib is allowed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - prints the program's name
+ * @argc: the number of args passed.
+ * @argv: arry of strings.
+ *
+ * Return: Always 0.
+ */
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	printf("%s", argv[1]);
+	return (0);
+}
