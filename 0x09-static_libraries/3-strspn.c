85b2ecaccf2974d987d848173516513e98a6a609
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-04 16:13:32 +0100

    compiled files

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring.
+ * @s: string.
+ * @accept: chars to check s with.
+ *
+ * Return: the length, otherwise -1.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	printf("In progress...\n%s\n%s", s, accept);
+	return (0);
+}
---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strpbrk - gets the length of a prefix substring.
+ * @s: string.
+ * @accept: chars to check s with.
+ *
+ * Return: the length, otherwise -1.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	printf("In progress...\n%s\n%s", accept, s);
+	return (s);
+}
