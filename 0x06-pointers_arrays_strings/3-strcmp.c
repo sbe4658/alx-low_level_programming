b065848c8300f4eca317e42732e1c00175119745
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-05 21:25:58 +0100

    Fixee

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,13 +4,14 @@
  * @s1: the first string.
  * @s2: the second string.
  *
- * Return: 0 if they're the same, otherwise the ascii diffrence between the two chars.
+ * Return: 0 if they're the same,
+ * otherwise the ascii diffrence between the two chars.
  */
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 != '\0')
 	{
-		if (s1 != s2)
+		if (*s1 != *s2)
 			return (*s1 - *s2);
 		s1++;
 		s2++;
