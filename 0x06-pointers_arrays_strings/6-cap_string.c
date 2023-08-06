609eea44b31d7e922e7a0db0a572532e7ae0d415
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-06 17:20:00 +0100

    The first letter is not capitalized

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string - capitalize words in a string.
+ * @s: a given string.
+ *
+ * Return: a pointer the rsultatent string.
+ */
+char *cap_string(char *s)
+{
+	char sep[] = " \t\n,;.!?\"(){}";
+	int i = 0, c = 0, csep;
+
+	while (s[i] != '\0')
+	{
+		csep = 0;
+		while (sep[csep] != '\0')
+		{
+			if (s[i] == sep[csep])
+			{
+				if (s[i + 1] <= 'z' && s[i + 1] >= 'a')
+				{
+					c = s[i + 1] - 'a';
+					s[i + 1] = c + 'A';
+				}
+			}
+			csep++;
+		}
+		i++;
+	}
+	return (s);
+}
