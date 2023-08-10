c6e99170a911d84b0f9b8f4f6a96581edaa16f5e
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-10 18:11:07 +0100

    Fixed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 		cat[i] = s1[i];
-	n++;
 	while (n != 0 && s2 != NULL)
 	{
 		cat[i] = *s2;
