6576ad056a346a48b216e6490cde102b80670c66
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-10 18:07:51 +0100

    Promising start

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * string_nconcat - contaconate two strings.
+ * @s1: 1st string.
+ * @s2: 2nd string.
+ * @n: n bytes to be copied from s2.
+ *
+ * Return: a pointer to a newly allocated space in mem, a string
+ * that have s1 and the first n bytes of s2.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1, len2, i = 0;
+	char *cat;
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	if (n > len2)
+		n = len2;
+	cat = malloc(sizeof(char) * (len1 + n) + 1);
+	if (cat == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		cat[i] = s1[i];
+	n++;
+	while (n != 0 && s2 != NULL)
+	{
+		cat[i] = *s2;
+		i++;
+		n--;
+		s2++;
+	}
+	cat[i] = '\0';
+	return (cat);
+}
+/**
+ * _strlen - gets the length of a string.
+ * @s: a pointer to a string.
+ *
+ * Return: The length of s.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,5 +5,7 @@
 #include <stdio.h>
 
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *, char *, unsigned int);
+int _strlen(char *);
 
 #endif
