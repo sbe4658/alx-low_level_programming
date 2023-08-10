601658814c033d488c002447d632a815ed1591fd
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-10 18:38:53 +0100

    I made a mistake

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,12 +20,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	cat = malloc(sizeof(char) * (len1 + n) + 1);
 	if (cat == NULL)
 		return (NULL);
-	if (s2 != NULL)
+	if (s1 != NULL)
 	{
 		for (i = 0; s1[i] != '\0'; i++)
 			cat[i] = s1[i];
 	}
-	if (s1 != NULL)
+	if (s2 != NULL)
 	{
 		while (n != 0)
 		{
