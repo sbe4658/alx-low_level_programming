0d492e0cef15bc06ab9d0f2e4515a13e813d965d
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-10 20:17:37 +0100

    Calloc finally

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _calloc - allocate memory for an array using.
+ * @nmemb: array elements.
+ * @size: size of nmemb.
+ *
+ * Return: a void pointer to a newly allocated space in memory.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *tmp;
+
+	tmp = malloc(size * nmemb);
+	if (tmp == NULL || size == 0 || nmemb == 0)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		tmp[i] = '\0';
+	return ((void *) tmp);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,9 +3,11 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *, char *, unsigned int);
 int _strlen(char *);
+void *_calloc(unsigned int, unsigned int);
 
 #endif
