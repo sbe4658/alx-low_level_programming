9da7af35303235de575215cfea1d1ce6e6447abb
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-10 14:46:02 +0100

    fixed betty

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,7 @@
  * malloc_checked - allocate memory using malloc.
  * @b: the number of bytes to allocate.
  *
- * Return: Nothing, but will terminate with status value of 98 if 
+ * Return: Nothing, but will terminate with status value of 98 if
  * malloc failed
  */
 void *malloc_checked(unsigned int b)
