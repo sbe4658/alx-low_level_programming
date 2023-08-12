53137e632a00bcbbd00ff0913971e539410ee28f
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-12 01:17:35 +0100

    Handle the case when the new size is 0 and ptr is not NULL

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,7 +25,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	}
 	holder = malloc(new_size);
-	if (holder == NULL || new_size == NULL)
+	if (holder == NULL || new_size == 0)
 	{
 		free(holder);
 		return (NULL);
