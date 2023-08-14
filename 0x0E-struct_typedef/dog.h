b0839836576b8dead43852969cfc1d9a1ddca513
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-14 21:37:33 +0100

    ~Woof!

---
+++ 0x0E-struct_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DODLE_H
+#define DODLE_H
+
+/**
+ * struct dog - dog's data for sorting i guess!
+ * @name: the name of the dog.
+ * @age: the age of the dog
+ * @owner: julein barbier don't ask me why.
+ *
+ * Description: This strcut is for the first task of this
+ *		project. The real question is
+ *		Wat da dawg doin'?!
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
