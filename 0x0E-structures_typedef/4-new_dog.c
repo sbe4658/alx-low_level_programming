53a0e19b96e391752e9f3cd0395e83b7d061b9ca
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-15 14:48:03 +0100

    am I supposed to free the char *?

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,11 +10,59 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dawg = malloc(sizeof(dog_t));
+	char *n, *o;
+	int len1, len2;
 
 	if (dawg == NULL)
+	{
+		free(dawg);
 		return (NULL);
-	dawg->name = name;
+	}
+	len1 = _strlen(name);
+	n = malloc(sizeof(char) * len1);
+	len2 = _strlen(owner);
+	o = malloc(sizeof(char) * len2);
+	if (n == NULL || o == NULL)
+	{
+		free(dawg->name);
+		free(dawg->owner);
+		free(dawg);
+		return (NULL);
+	}
+	dawg->name = _strcpy(n, name);
 	dawg->age = age;
-	dawg->owner = owner;
+	dawg->owner = _strcpy(o, owner);
 	return (dawg);
+}
+/**
+ * _strcpy - copies a string.
+ * @dest: destination.
+ * @src: source.
+ *
+ * Return: Nothing.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	return (dest);
+}
+/**
+ * _strlen - gets string length.
+ * @s: a pointer to a string.
+ *
+ * Return: the length of s.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	if (s != NULL)
+	{
+		while (s[len] != '\0')
+			len++;
+	}
+	return (len);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -28,6 +28,8 @@ void print_dog(struct dog *);
 void check_for_null(struct dog *);
 dog_t *new_dog(char *, float, char *);
 void free_dog(dog_t *d);
+char *_strcpy(char *, char *);
+int _strlen(char *);
 
 /* Headers section */
 #include <stdlib.h>
