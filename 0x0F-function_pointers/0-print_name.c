2302e605a97c1d7e8393596a26595865743a6e91
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-16 16:46:18 +0100

    Fixed spelling

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a given name.
+ * @name: the name to print.
+ * @f: a function pointer.
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#ifndef FP_H
+#define FP_H
+
+/* Function's section */
+void print_name(char *, void (*)(char *));
+void print_name_uppercase(char *name);
+void print_name_as_is(char *name);
+
+/* Header's section */
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
