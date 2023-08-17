8963077861003fc67a096862ea74251d99d6a061
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-18 00:15:41 +0100

    4 if statements, be blind pls

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,84 @@
+#include "variadic_functions.h"
+/**
+ * print_all - prints anything.
+ * @format: list of types of arguments.
+ *
+ * Return: Nothing.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ar;
+	int i, c;
+	type_t ts[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_f},
+		{'s', print_s}
+	};
+
+	va_start(ar, format);
+	i = 0;
+	while (format[i] != '\0')
+	{
+		c = 0;
+		while (c < 4)
+		{
+			if (ts[c].t == format[i])
+			{
+				ts[c].f(ar);
+				if (format[i + 1] != '\0')
+					printf(", ");
+
+			}
+			c++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ar);
+}
+/**
+ * print_char - prints a char.
+ * @ar: argument of type int.
+ *
+ * Return: Nothing.
+ */
+void print_char(va_list ar)
+{
+	printf("%c", va_arg(ar, int));
+}
+/**
+ * print_int - prints an int.
+ * @ar: argument of type int.
+ *
+ * Return: Nothing.
+ */
+void print_int(va_list ar)
+{
+	printf("%d", va_arg(ar, int));
+}
+/**
+ * print_f - prints a float.
+ * @ar: argument of type float.
+ *
+ * Return: Nothing.
+ */
+void print_f(va_list ar)
+{
+	printf("%f", va_arg(ar, double));
+}
+/**
+ * print_s - prints a string.
+ * @ar: argument of type char *.
+ *
+ * Return: Nothing.
+ */
+void print_s(va_list ar)
+{
+	char *tmp;
+
+	tmp = va_arg(ar, char *);
+	if (tmp == NULL)
+		tmp = "(nil)";
+	printf("%s", tmp);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,10 +11,28 @@
  */
 typedef unsigned int ui_t;
 
+/**
+ * struct types - types struct.
+ *
+ * @t: characater.
+ * @f: function pointer.
+ */
+typedef struct types
+{
+	char t;
+	void (*f)(va_list);
+} type_t;
+
 /* Function's section */
 int _putchar(char);
 int sum_them_all(const unsigned int, ...);
-void print_numbers(const char *, const unsigned int, ...);
+void print_numbers(const char *, const ui_t, ...);
 void print_strings(const char *, const ui_t, ...);
+void print_all(const char * const, ...);
+void print_char(va_list);
+void print_int(va_list);
+void print_f(va_list);
+void print_s(va_list);
+void (*search(char))(va_list);
 
 #endif
