83687fd54362fc3433a81f87d185517e60d88080
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-24 22:33:31 +0100

    Add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node - adds a node at the beginning of a linked list.
+ * @head: the old head of list_t list.
+ * @str: a struct member.
+ *
+ * Return: a pointer the Head of list_t list.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *tmp;
+
+	tmp = malloc(sizeof(list_t));
+	if (tmp == NULL)
+		return (NULL);
+	tmp->str =  strdup(str);
+	tmp->len = _strlen(str);
+	tmp->next = NULL;
+	if (*head == NULL)
+		*head = tmp;
+	else
+	{
+		tmp->next = *head;
+		(*head) = tmp;
+	}
+	return (*head);
+}
+/**
+ * _strlen - computes the length of s.
+ * @s: a string.
+ *
+ * Return: the length of s.
+ */
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	if (s != NULL)
+		while (*(s++))
+			len++;
+
+	return (len);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,7 +21,9 @@ typedef struct list_s
 } list_t;
 
 /* function's section */
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
+size_t print_list(const list_t *);
+size_t list_len(const list_t *);
+list_t *add_node(list_t **, const char *);
+int _strlen(const char *);
 
 #endif
