367c21ecdcef767fd9f4a2cbc6f8105f1c93bec1
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-25 01:09:33 +0100

    Add the prototype to lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,5 +25,6 @@ size_t print_list(const list_t *);
 size_t list_len(const list_t *);
 list_t *add_node(list_t **, const char *);
 int _strlen(const char *);
+list_t *add_node_end(list_t **, const char *);
 
 #endif
