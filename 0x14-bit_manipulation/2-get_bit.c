49d41a97bc1fee2cc65f7e9d094dcb7947d3f8c5
Alpha 00 <salekbenelhabchi@gmail.com>
2023-08-31 23:45:01 +0100

    Update get_bit function$

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned int i;
 
 	if (n == 0)
-		return (0);
+		return ((index == 0 ? 0 : -1));
 	for (i = 0; i < index; i++)
 	{
 		n >>= 1;
