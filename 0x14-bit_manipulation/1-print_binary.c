d4af3fc98a7bc65b93d5cea3a988c1c3c31e2de5
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-31 23:13:44 +0100

    Update print_binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,17 +7,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int m = 0;
-
-	if (n == 0)
-		_putchar('0');
-	else
-	{
-		while (n != 0)
-		{
-			m = n % 2;
-			_putchar(m + '0');
-			n /= 2;
-		}
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
