43be5193748f14ee7910455dbc08fbfebee851b7
Alpha 00 <salekbenelhabchi@gmail.com>
2023-09-01 16:06:55 +0100

    Fixed!

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,6 +12,8 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	if (n == 0)
 		return ((index == 0 ? 0 : -1));
+	if (index > 32)
+		return (-1);
 	for (i = 0; i < index; i++)
 	{
 		n >>= 1;
