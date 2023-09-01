b218cbe7aa70d0215cb5f6690ac3050c003d1685
Alpha 00 <salekbenelhabchi@gmail.com>
2023-09-01 18:10:08 +0100

    Logical operator are a bit confusing

---
+++ 0x14-bit_manipulation/5-flp_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * flip_bit - computes the number of needed to get from m to n.
+ * @n: first number.
+ * @m: second number.
+ *
+ * Return: return the number of flips needed.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int flips = 0;
+
+	while (n != 0 || m != 0)
+	{
+		if ((n & 1) ^ (m & 1))
+			flips++;
+		n >>= 1;
+		m >>= 1;
+	}
+	return (flips);
+}
