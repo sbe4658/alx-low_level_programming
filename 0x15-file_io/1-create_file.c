b473ab186ee0827a1e23a082f0108e9d6e3c761b
Alpha 00 <salekbenelhabchi@gmail.com>
2023-09-05 00:33:22 +0100

    Fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,12 +25,13 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t s = 0;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
-	s = write(fd, text_content, _strlen(text_content));
+	if (text_content)
+		s = write(fd, text_content, _strlen(text_content));
 	if (s == -1)
 		return (-1);
 	close(fd);
