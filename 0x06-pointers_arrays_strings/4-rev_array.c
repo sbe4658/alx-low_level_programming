89bdd14021873f15a295ea2c09d18f561b454d74
Alpha 00 <salekbenelhabchi@gmail.com>
2023-09-24 16:02:40 +0100

    rev array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,32 +8,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i = 0, m = 0, c = 0;
-	int tmp[1000];
+	int i = 0, tmp = 0;
 
-	n--;
-	while (c <= n / 2)
+	while (i < n / 2)
 	{
-		tmp[c] = a[c];
-		printf("tmp[%d]: %d\n", c, tmp[c]);
-		c++;
-	}
-	printf("%d\n", c);
-	while (i <= n)
-	{
-		if (i < n / 2)
-		{
-			m = a[n - i];
-			a[i] = m;
-		}
-		else if (i == n / 2)
-			i++;
-		else
-		{
-			a[i] = tmp[c];
-			c--;
-		}
-		printf("a[%d]: %d\n", i, tmp[i]);
+		tmp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = tmp;
 		i++;
 	}
 }
