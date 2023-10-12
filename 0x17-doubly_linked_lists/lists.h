2b1c619779c09905faac7f022a4e631b194c4c51
Alpha 00 <salekbenelhabchi@gmail.com>
2023-10-12 17:48:52 +0100

    Fixed

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -23,6 +23,6 @@ size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **h, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **h, const int n);
-void free_dnodeint(dlistint_t *h);
+void free_dlistint(dlistint_t *h);
 
 #endif
