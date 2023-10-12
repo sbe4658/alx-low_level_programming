69999eb2feecb8c16e21462bb571992f78d74aa4
Mr. Bread <salekbenelhabchi@gmail.com>
2023-10-12 20:17:56 +0100

    The singly linked list is just better, more simple and efficent

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - deletes node at a given index.
+ * @head: the head node.
+ * @index: the index.
+ *
+ * Return: 1 success, -1 fail.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, int index)
+{
+	dlisint_t *tmp, *h = *head;
+
+	if (h)
+	{
+		if (index)
+		{
+			for (; index; index--)
+			{
+				if (!h)
+					return (-1);
+				h = h->next;
+			}
+			tmp = h->prev;
+			h->next->prev = tmp;
+			tmp->next = h->next;
+		}
+		else
+		{
+			(*h) = (*h)->next;
+			(*h)->prev = NULL;
+		}
+	}
+	free(h);
+	return (1);
+}
