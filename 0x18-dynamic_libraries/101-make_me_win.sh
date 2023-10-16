0af667c04a10706c64af05de3f0630db7cc662c8
Alpha 00 <salekbenelhabchi@gmail.com>
2023-10-16 21:32:04 +0100

    Finally

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -P ~/ ""
-export LD_PRELOAD="~/
+wget -qP /tmp "https://github.com/sbe4658/alx-low_level_programming/raw/main/0x18-dynamic_libraries/code_injection/libc.so.6"
+export LD_PRELOAD="/tmp/libc.so.6"
+echo $LD_PRELOAD
