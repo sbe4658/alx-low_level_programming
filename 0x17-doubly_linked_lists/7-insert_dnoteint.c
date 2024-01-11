4d309eb0b0a7c8fc85cb8a6e33a0b9ad184de407
Alpha 00 <salekbenelhabchi@gmail.com>
2024-01-11 17:02:37 +0100

    Without testing?

---
+++ 0x17-doubly_linked_lists/7-insert_dnoteint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given index.
+ * @h: the head node.
+ * @idx: the index.
+ * @n: integer/element of the new node.
+ *
+ * Return: The address of the new node, otherwise NULL.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, u_int idx, int n)
+{
+	dlistint_t *curr = *h, *new;
+
+	if (idx)
+	{
+		new = malloc(sizeof(dlistint_t));
+		if (!new)
+			return (NULL);
+		new->n = n;
+		for (; idx; idx--)
+		{
+			if (!curr)
+				return (NULL);
+			curr = curr->next;
+		}
+		curr->prev->next = new;
+		curr->prev = new;
+		new->next = curr;
+	}
+	else
+		new = add_dnodeint(h, n);
+	return (new);
+}
