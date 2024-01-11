b7c460ebb7f8ea0041619f03a9cfdce6796a53eb
Alpha 00 <salekbenelhabchi@gmail.com>
2024-01-11 18:20:44 +0100

    FREE

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, u_int idx, int n)
 		for (; idx; idx--)
 		{
 			if (!curr)
+			{
+				free(new);
 				return (NULL);
+			}
 			curr = curr->next;
 		}
 		curr->prev->next = new;
