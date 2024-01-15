cb021e99cb5d31786f5c09d3dc2249b8b1e2f81a
Alpha 00 <salekbenelhabchi@gmail.com>
2024-01-15 16:07:49 +0100

    Fixed

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -OL "https://github.com/sbe4658/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libdynamic.so"
-export LD_PRELOAD=./libc.so.6
+curl -OL "https://github.com/sbe4658/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libc.so.6" -o ../
+export LD_PRELOAD=../libc.so.6
