4a634351475456e0118c9573888abfa6da6dde1f
Alpha 00 <salekbenelhabchi@gmail.com>
2024-01-15 15:02:12 +0100

    Ctypes

---
+++ 0x18-dynamic_libraries/source_code/operation.c
@@ -0,0 +1,55 @@
+/**
+ * add - returns the results.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: the result.
+ */
+int add(int a, int b)
+{
+	return a + b;
+}
+/**
+ * sub - returns the results.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: the result.
+ */
+int sub(int a, int b)
+{
+	return a - b;
+}
+/**
+ * mul - returns the results.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: the result.
+ */
+int mul(int a, int b)
+{
+	return a * b;
+}
+/**
+ * div - returns the results.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: the result.
+ */
+int div(int a, int b)
+{
+	return a / b;
+}
+/**
+ * mod - returns the results.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: the result.
+ */
+int mod(int a, int b)
+{
+	return a % b;
+}
