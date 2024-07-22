fcaea7fcf796205affae634979f0c77b5487bf35
Alpha 01 <salekbenelhabchi@gmail.com>
2024-07-22 17:22:43 +0100

    I think I made a great job!

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: hash table, a fixed size array.
+ * @key: string.
+ * @value: value pair with key.
+ *
+ * Return: On success 1; otherwise 0.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned int index = 0;
+	hash_node_t *new, *position;
+
+	if (!key)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	position = ht->array[index];
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (0);
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = NULL;
+	if (position)
+	{
+		while (position->next)
+			position = position->next;
+		position->next = new;
+	}
+	else
+		position = new;
+	return (1);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -38,5 +38,6 @@ typedef struct hash_table_s
 hash_table_t *hash_table_create(unsigned int);
 unsigned long int hash_djb2(const unsigned char *);
 unsigned long int key_index(const unsigned char *, unsigned long int);
+int hash_table_set(hash_table_t *, const char *, const char *);
 
 #endif
