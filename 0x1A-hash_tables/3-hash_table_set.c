ca28e68a45e9f9162d3769167cec023a6df68d76
Alpha 01 <salekbenelhabchi@gmail.com>
2024-07-22 22:04:52 +0100

    Optimized

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,9 +16,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	position = &(ht->array[index]);
+	if (*position)
+	{
+		if (strcmp((*position)->key, key) == 0)
+		{
+			(*position)->value = strdup(value);
+			return (1);
+		}
+	}
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
+	{
+		free(new);
 		return (0);
+	}
 	new->key = strdup(key);
 	new->value = strdup(value);
 	new->next = NULL;
